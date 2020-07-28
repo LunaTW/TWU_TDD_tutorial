75fc3bcc545526687611f13cf6049c708bb0a571
LunaTW <yue.zhang1@thoughtworks.com>
2020-07-29 01:50:20 +0800

    factorial_pass3

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,19 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if(i==1){
+            return 1;
+        }else if(i==2){
+            return 2;
+        }else if(i==0){
+            return 1;
+        }
         return -1;
     }
+    //如果是1是要求返回1
+    // 如果是2时要求返回2
+    // 如果是0时要返回1
+    // 如果是3时，返回6
+    //如果是负数应该抛出 Illegal Argument Exception
+
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/factorial/FactorialTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/factorial/FactorialTests.java
@@ -20,31 +20,31 @@ public class FactorialTests {
     private final Factorial factorial = new Factorial();
 
     @Test // Make me pass first
-    @Ignore // Remove each @Ignore to allow the test to run
+    //如果是1是要求返回1
     public void shouldReturnOneWhenNumberIsOne(){
         assertThat(new Factorial().compute(1), is(1));
     }
 
     @Test
-    @Ignore // Remove each @Ignore to allow the test to run
+    // 如果是2时要求返回2
     public void shouldReturnTwoWhenNumberIsTwo(){
         assertThat(factorial.compute(2), is(2));
     }
 
     @Test
-    @Ignore // Remove each @Ignore to allow the test to run
+    // 如果是0时要返回1
     public void shouldReturnOneWhenNumberIsZero(){
         assertThat(factorial.compute(0), is(1));
     }
 
     @Test
-    @Ignore // Remove each @Ignore to allow the test to run
+    // 如果是3时，返回6
     public void shouldReturnSixWhenNumberIsThree(){
         assertThat(factorial.compute(3), is(6));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    @Ignore // Remove each @Ignore to allow the test to run
+    //如果是负数应该抛出 Illegal Argument Exception
     public void shouldThrowIllegalArgumentExceptionWhenNumberIsNegative(){
         factorial.compute(-1);
     }
